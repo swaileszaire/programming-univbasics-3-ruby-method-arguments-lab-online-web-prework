fb848efbe5a58c9ef3d078e2b6da3404b859c669
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-10 04:27:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,8 +6,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language)
-  language = Ruby 
+def introduction_with_language_optional(name, Ruby)
   puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
 
