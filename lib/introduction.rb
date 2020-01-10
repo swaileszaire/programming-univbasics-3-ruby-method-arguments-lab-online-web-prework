7fc211eb6b3675a7ceda3d9e2e4af5eca32bd209
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-10 04:21:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,11 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction
 
 
 
